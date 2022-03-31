5a745c79290d60a981c7cf28689343cffdc3f7fb
jesusElDeTic <74316857+jesusElDeTic@users.noreply.github.com>
2022-04-01 00:15:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    if (boton == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (boton == 1) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+let boton = 0
 basic.showLeds(`
     . . . . .
     . # . # .
@@ -7,14 +22,15 @@ basic.showLeds(`
     `)
 basic.pause(500)
 let cuenta = 3
-let tiempo = 0
 for (let index = 0; index < 3; index++) {
     basic.showNumber(cuenta)
     cuenta = cuenta - 1
     basic.pause(100)
 }
+basic.clearScreen()
 basic.showString("Go!")
-let boton = randint(0, 1)
+basic.clearScreen()
+boton = randint(0, 1)
 if (boton == 0) {
     basic.showLeds(`
         # . . . .
